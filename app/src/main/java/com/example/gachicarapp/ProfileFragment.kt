1db84095cfcd53e5df528659301682c590e26776
yeonsu <bestdustn@sookmyung.ac.kr>
2024-03-28 17:35:18 +0900

    [FIX] Update getUserReports - 주행 리포트 조회 API 응답 받아오는 방법 변경

--- app/src/main/java/com/example/gachicarapp/ProfileFragment.kt
+++ app/src/main/java/com/example/gachicarapp/ProfileFragment.kt
@@ -8,10 +8,13 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.gachicarapp.databinding.FragmentProfileBinding
 import com.example.gachicarapp.retrofit.RetrofitConnection
+import com.example.gachicarapp.retrofit.response.ApiResponse
+import com.example.gachicarapp.retrofit.response.CarAndReport
 import com.example.gachicarapp.retrofit.response.DriveReport
 import com.example.gachicarapp.retrofit.response.GetGroupInfo
 import com.example.gachicarapp.retrofit.service.AppServices
 import com.example.gachicarapp.retrofit.service.GroupService
+import com.example.gachicarapp.retrofit.service.ReportService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -40,8 +43,6 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         getGroupData()
-
-        val retrofitAPI = RetrofitConnection.getInstance(requireContext()).create(AppServices::class.java)
 
         binding.inviteNewMember.setOnClickListener {
             val intent = Intent(activity, EditMActivity::class.java)
@@ -95,22 +96,25 @@ class ProfileFragment : Fragment() {
     }
 
     private fun getReportData() {
-        val retrofitAPI = RetrofitConnection.getInstance(requireContext()).create(AppServices::class.java)
+        val retrofitAPI = RetrofitConnection.getInstance(requireContext()).create(ReportService::class.java)
 
         // API 호출
         retrofitAPI.getDriveReport()
-            .enqueue(object : Callback<DriveReport> {
-                override fun onResponse(call: Call<DriveReport>, response: Response<DriveReport>) {
+            .enqueue(object : Callback<ApiResponse<DriveReport>> {
+                override fun onResponse(
+                    call: Call<ApiResponse<DriveReport>>,
+                    response: Response<ApiResponse<DriveReport>>
+                ) {
                     if (response.isSuccessful) {
                         // API 응답이 성공적으로 수신된 경우 UI 업데이트를 수행합니다.
-                        response.body()?.let { updateReportUI(it) }
+                        response.body()?.data?.let { updateReportUI(it) }
                     } else {
                         // API 응답이 실패한 경우 에러 메시지를 출력합니다.
                         Toast.makeText(requireContext(), "리포트 데이터를 가져오는 데 실패했습니다.", Toast.LENGTH_SHORT).show()
                     }
                 }
 
-                override fun onFailure(call: Call<DriveReport>, t: Throwable) {
+                override fun onFailure(call: Call<ApiResponse<DriveReport>>, t: Throwable) {
                     // API 호출이 실패한 경우 에러 메시지를 출력합니다.
                     Toast.makeText(requireContext(), "API 호출 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
                     t.printStackTrace()
@@ -130,15 +134,15 @@ class ProfileFragment : Fragment() {
         binding.tvCarNickname.text = carNickName
 
     }
-    private fun updateReportUI(reportQualityData: DriveReport) {
-        val userName = reportQualityData.data.userName
-        val carName = reportQualityData.data.car.carName
-        val carNumber = reportQualityData.data.car.carNumber
-        val departure = reportQualityData.data.departure
-        val destination = reportQualityData.data.destination
-        val driveTime = reportQualityData.data.driveTime
-        val startTime = reportQualityData.data.startTime
-        val endTime = reportQualityData.data.endTime
+    private fun updateReportUI(report: DriveReport) {
+        val userName = report.userName
+        val carName = report.car.carName
+        val carNumber = report.car.carNumber
+        val departure = report.departure
+        val destination = report.destination
+        val driveTime = report.driveTime
+        val startTime = report.startTime
+        val endTime = report.endTime
 
 
         binding.carNumber.text = carNumber
--- app/src/main/java/com/example/gachicarapp/retrofit/service/AppServices.kt.kt
+++ app/src/main/java/com/example/gachicarapp/retrofit/service/AppServices.kt.kt
@@ -1,8 +1,8 @@
 package com.example.gachicarapp.retrofit.service
 
+import com.example.gachicarapp.retrofit.response.ApiResponse
 import com.example.gachicarapp.retrofit.response.DriveReport
 import com.example.gachicarapp.retrofit.response.ReserveData
-import com.example.gachicarapp.retrofit.response.getCarInfo
 import com.example.gachicarapp.retrofit.response.statistic_r_Response
 import retrofit2.Call
 import retrofit2.http.*
@@ -17,8 +17,5 @@ interface AppServices {
     // 통계 관련 API
     @GET("/api/car/fuel")
     fun getFuelData(): Call<statistic_r_Response>
-
-    @GET("/api/report")
-    fun getDriveReport(): Call<DriveReport>
   
 }
